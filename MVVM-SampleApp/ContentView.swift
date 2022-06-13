cfe2db693ee33a9f2335e21d18c3814b695bcce2
Kiran Poudel <kp.krn25@gmail.com>
2022-06-13 13:41:47 +0545

    Initial Commit

---
+++ MVVM-SampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM-SampleApp
+//
+//  Created by Kiran Poudel on 6/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM-SampleApp/MVVM_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SampleAppApp.swift
+//  MVVM-SampleApp
+//
+//  Created by Kiran Poudel on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
